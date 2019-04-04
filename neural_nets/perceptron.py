b1a2e7f2a27516c3bd1ada5b1459767db49ff377
Oppac <oppac26@gmail.com>
2019-04-04 15:53:39 +0200

    moving line

--- neural_nets/perceptron.py
+++ neural_nets/perceptron.py
@@ -5,7 +5,7 @@ import numpy as np
 class Perceptron:
     def __init__(self):
         self.weights = np.random.choice([-1, 1], 2)
-        self.learning_rate = 0.1
+        self.learning_rate = 0.05
 
     def guess(self, input):
         sum = 0
@@ -19,30 +19,35 @@ class Perceptron:
         for i in range(len(self.weights)):
             self.weights[i] += error * input[i] * self.learning_rate
 
-def show(x, y, labels):
+#####################################################
+
+def line(x):
+    return 0.3 * x - 1
+
+def show(x, y, labels, p):
     colors = ['red', 'blue']
     plt.scatter(x=x, y=y, s=30, marker='o', c=labels, edgecolors='black',
                 cmap=matplotlib.colors.ListedColormap(colors))
-    plt.plot([0,800], [0,800], "k-")
+    plt.plot(x, line(x) , "k-")
     plt.show()
 
 def main():
-    size = 100
+    size = 200
     width = 800; height = 800
     p = Perceptron()
 
     x = np.random.randint(0, width, size=size)
     y = np.random.randint(0, height, size=size)
-    labels = [1 if x[i] > y[i] else -1 for i in range(size)]
+    labels = [1 if y[i] > line(x[i]) else -1 for i in range(size)]
     inputs = [[x[i], y[i]] for i in range(size)]
 
-    show(x, y, [p.guess(inputs[i]) for i in range(size)])
+    show(x, y, [p.guess(inputs[i]) for i in range(size)], p)
     while True:
         guesses = []
         for i in range(size):
             p.train(inputs[i], labels[i])
             guesses.append(p.guess(inputs[i]))
-        show(x, y, guesses)
+        show(x, y, guesses, p)
         if guesses == labels:
             break
 
