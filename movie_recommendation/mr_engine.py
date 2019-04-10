2afdfe36fb3745bc507dcada53cbcf39b0d2a32f
Oppac <oppac26@gmail.com>
2019-04-10 22:31:14 +0200

    euclidian similarity

--- movie_recommendation/mr_engine.py
+++ movie_recommendation/mr_engine.py
@@ -1,13 +1,37 @@
 # Dataset by MovieLens: https://grouplens.org/datasets/movielens/
 
 import csv
+import math
 import sys
 
 
 def euclidian_similarity(user_ratings, user_1, user_2):
-    print(list(user_ratings.values())[user_1])
-    print()
-    print(list(user_ratings.values())[user_2])
+    sum_squares = 0
+    common = common_movies(user_ratings, user_1, user_2)
+    print(common)
+    if not common:
+        return 0
+    for i in common:
+        diff = i[1] - i[2]
+        sum_squares += diff * diff
+    sqrt_diff = math.sqrt(sum_squares)
+    similarity = 1 / (1 + sqrt_diff)
+    return similarity
+
+
+def common_movies(user_ratings, user_1, user_2):
+    movies_1 = {}
+    movies_2 = {}
+    common = []
+    for i in list(user_ratings.values())[user_1]:
+        movies_1[i[0]] = i[1]
+    for i in list(user_ratings.values())[user_2]:
+        movies_2[i[0]] = i[1]
+    for i in movies_1.keys():
+        if i in movies_2.keys():
+            common.append([i, movies_1.get(i), movies_2.get(i)])
+    return common
+
 
 def read_by_user():
     user_ratings = {}
@@ -25,12 +49,12 @@ def main():
     user_ratings = read_by_user()
     keys = list(user_ratings.keys())
     if len(sys.argv) == 3:
-        user_1 = int(keys[int(sys.argv[1])-2])
-        user_2 = int(keys[int(sys.argv[2])-2])
+        user_1 = int(keys[int(sys.argv[1])-1])
+        user_2 = int(keys[int(sys.argv[2])-1])
     else:
         user_1 = int(keys[0])
         user_2 = int(keys[1])
-    euclidian_similarity(user_ratings,  user_1, user_2)
+    print(euclidian_similarity(user_ratings, user_1, user_2))
 
 
 main()
