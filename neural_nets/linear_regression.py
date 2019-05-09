75a3c8ba1440cd067b1bf0bba386d4fd75b25492
Oppac <oppac26@gmail.com>
2019-05-09 23:31:16 +0200

    final

--- neural_nets/linear_regression.py
+++ neural_nets/linear_regression.py
@@ -1,3 +1,4 @@
+import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -19,33 +20,35 @@ def linear_regression(x_data, y_data):
     b = y_mean - m * x_mean
     return m, b
 
+def new_plot(x_data, y_data, plt):
+    plt.clf()
+    plt.scatter(x=x_data, y=y_data, s=30, marker='o', edgecolors="black")
+    m, b = linear_regression(x_data, y_data)
+    plt.plot(x_data, m*x_data+b, '-r')
+    plt.draw()
+
 def onclick(event):
     global x_data, y_data
     x_data = np.append(x_data, event.xdata)
     y_data = np.append(y_data, event.ydata)
-    m, b = linear_regression(x_data, y_data)
-    plt.clf()
-    plt.scatter(x=x_data, y=y_data, s=25, marker='o', edgecolors="black")
-    plt.plot(x_data, m*x_data+b, '-r')
-    plt.draw()
+    new_plot(x_data, y_data, plt)
 
 def show(x_data, y_data):
     fig, ax = plt.subplots()
-    ax.scatter(x=x_data, y=y_data, s=25, marker='o', edgecolors="black")
-
-    m, b = linear_regression(x_data, y_data)
-    plt.plot(x_data, m*x_data+b, '-r')
-
+    new_plot(x_data, y_data, plt)
     fig.canvas.mpl_connect('button_press_event', onclick)
     plt.show()
 
 def main():
     global x_data, y_data
-    size = 2
-    x_data = np.random.normal(6, 3, size)
-    y_data = np.random.logistic(20, 4, size)
-    show(x_data, y_data)
+    if len(sys.argv) > 1:
+        size = int(sys.argv[1])
+    else:
+        size = 10
 
+    x_data = np.random.normal(6, 3, size)
+    y_data = np.random.normal(6, 3, size)
+    show(x_data, y_data)
 
 
 main()
