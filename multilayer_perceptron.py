3c44c00d11154c1787089568c813c37672769c69
Oppac <oppac26@gmail.com>
2019-05-21 19:16:05 +0200

    new init

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -1,31 +1,59 @@
 from matrix import Matrix
 
 class MutlilayerPerceptron:
-    def __init__(self, nb_inputs, nb_hidden, nb_outputs, learning_rate=0.1):
-        self.learning_rate = learning_rate
-        self.input_nodes = nb_inputs
-        self.hidden_nodes = nb_hidden
-        self.output_nodes = nb_outputs
-        self.weights_init()
+    "Layers: [nb_input_nodes, nb_output_nodes, nb_hidden_nodes, nb_hidden_layers]"
+    def __init__(self, layers, learning_rate=0.1):
+        if len(layers) == 4:
+            self.learning_rate = learning_rate
+            self.input_nodes = layers[0]
+            self.output_nodes = layers[1]
+            self.hidden_nodes = layers[2]
+            self.hidden_layers = layers[3]
+            self.weight_matrices = self.weights_init()
+            self.bias_vectors = self.bias_init()
+        else:
+            exit("Invalid layer size: [nb_input_nodes, nb_output_nodes" +
+            "nb_hidden_nodes, nb_hidden_layers]")
+
+    def new_matrix(self, nb_rows, nb_cols):
+        w_matrix = Matrix(nb_rows, nb_cols)
+        w_matrix.randomize(-1, 1)
+        return w_matrix
 
     def weights_init(self):
-        self.w_input_hidden = Matrix(self.hidden_nodes, self.input_nodes)
-        self.w_input_hidden.randomize(-1, 1)
-        self.w_hidden_output = Matrix(self.output_nodes, self.hidden_nodes)
-        self.w_hidden_output.randomize(-1, 1)
+        weight_matrices = []
 
-        self.bias_hidden = Matrix(self.hidden_nodes, 1)
-        self.bias_hidden.randomize(-1, 1)
-        self.bias_output = Matrix(self.output_nodes, 1)
-        self.bias_output.randomize(-1, 1)
+        w_input_hidden = self.new_matrix(self.hidden_nodes, self.input_nodes)
+        weight_matrices.append(w_input_hidden)
+
+        for _ in range(self.hidden_layers-1):
+            w_hidden_hidden = self.new_matrix(self.hidden_nodes, self.hidden_nodes)
+            weight_matrices.append(w_hidden_hidden)
+
+        w_hidden_output = self.new_matrix(self.output_nodes, self.hidden_nodes)
+        weight_matrices.append(w_hidden_output)
+
+        return weight_matrices
+
+    def bias_init(self):
+        bias_vectors = []
+        for _ in range(self.hidden_layers):
+            hidden_bias = self.new_matrix(self.hidden_nodes, 1)
+            bias_vectors.append(hidden_bias)
+
+        output_bias = self.new_matrix(self.output_nodes, 1)
+        bias_vectors.append(output_bias)
+        return bias_vectors
+
+################################################################################
 
     def feedforward(self, inputs):
-        hidden_guess = self.w_input_hidden.multiply_matrices(inputs)
-        hidden_guess = hidden_guess.add_vector(self.bias_hidden)
+        hidden_guess = self.weight_matrices[0].multiply_matrices(inputs)
+        hidden_guess = hidden_guess.add_vector(self.bias_vectors[0])
         hidden_guess = hidden_guess.apply_sigmoid()
 
-        output_guess = self.w_hidden_output.multiply_matrices(hidden_guess)
-        output_guess = output_guess.add_vector(self.bias_output)
+        output_guess = self.weight_matrices[-1].multiply_matrices(hidden_guess)
+        output_guess = output_guess.add_vector(self.bias_vectors[-1])
         output_guess = output_guess.apply_sigmoid()
         return output_guess, hidden_guess
 
@@ -42,21 +70,21 @@ class MutlilayerPerceptron:
         return gradient
 
     def backpropagation(self, output_guess, output_errors, hidden_guess, inputs):
-        w_hidden_output_T = self.w_hidden_output.transpose()
+        w_hidden_output_T = self.weight_matrices[-1].transpose()
         hidden_errors = w_hidden_output_T.multiply_matrices(output_errors)
 
         gradient_ouput = self.get_gradient(output_guess, output_errors)
-        self.bias_output = self.bias_output.add_matrices(gradient_ouput)
+        self.bias_vectors[-1] = self.bias_vectors[-1].add_matrices(gradient_ouput)
         gradient_hidden = self.get_gradient(hidden_guess, hidden_errors)
-        self.bias_hidden = self.bias_hidden.add_matrices(gradient_hidden)
+        self.bias_vectors[0] = self.bias_vectors[0].add_matrices(gradient_hidden)
 
         hidden_T = hidden_guess.transpose()
         deltaW_output = gradient_ouput.multiply_matrices(hidden_T)
-        self.w_hidden_output = self.w_hidden_output.add_matrices(deltaW_output)
+        self.weight_matrices[-1] = self.weight_matrices[-1].add_matrices(deltaW_output)
 
         inputs_T = inputs.transpose()
         deltaW_hidden = gradient_hidden.multiply_matrices(inputs_T)
-        self.w_input_hidden = self.w_input_hidden.add_matrices(deltaW_hidden)
+        self.weight_matrices[0] = self.weight_matrices[0].add_matrices(deltaW_hidden)
 
 
     def train(self, input_data, correct_outputs):
