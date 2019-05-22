7668f99b375d36fb81fa4e1a4c50dde2240b4d0b
Oppac <oppac26@gmail.com>
2019-05-22 20:53:07 +0200

    0.5 --'

--- xor.py
+++ xor.py
@@ -6,14 +6,19 @@ xor_data = [ [[1], [0]], [[0], [1]], [[0], [0]], [[1], [1]] ]
 correct_outputs = [ [[1]], [[1]], [[0]], [[0]] ]
 
 layers = [2, 1, 2, 1]
-learning_rate = 0.01
+learning_rate = 0.1
 neural_net = MutlilayerPerceptron(layers, learning_rate)
 
-test = Matrix()
-test.give_values([[0], [0]])
-
-for i in range(5000):
+for i in range(10000):
     input_rand = random.choice(xor_data)
     output_rand = correct_outputs[xor_data.index(input_rand)]
     neural_net.train(input_rand, output_rand)
-print(neural_net.feedforward(test)[-1].values)
+
+for i in range(len(xor_data)):
+    inputs = Matrix(2, 1)
+    inputs.give_values(xor_data[i])
+    print(inputs.values)
+    print(neural_net.feedforward(inputs)[-1].values)
+    print()
+print()
+print([neural_net.weight_matrices[i].values for i in range(len(neural_net.weight_matrices))])
