d80637b3bf3b5296798d867efa0086a7d7dd4677
Oppac <oppac26@gmail.com>
2019-05-25 06:29:43 +0200

    why?

--- xor.py
+++ xor.py
@@ -6,19 +6,18 @@ xor_data = [ [[1], [0]], [[0], [1]], [[0], [0]], [[1], [1]] ]
 correct_outputs = [ [[1]], [[1]], [[0]], [[0]] ]
 
 layers = [2, 2, 1, 1]
-learning_rate = 0.5
+learning_rate = 0.1
 neural_net = MutlilayerPerceptron(layers, learning_rate)
 
-for i in range(10000):
+for i in range(1000):
     input_rand = random.choice(xor_data)
     output_rand = correct_outputs[xor_data.index(input_rand)]
     neural_net.train(input_rand, output_rand)
 
+print()
 for i in range(len(xor_data)):
     inputs = Matrix(2, 1)
     inputs.give_values(xor_data[i])
     print(inputs.values)
     print(neural_net.feedforward(inputs)[-1].values)
     print()
-print()
-print([neural_net.weight_matrices[i].values for i in range(len(neural_net.weight_matrices))])
